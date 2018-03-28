279f6b726b81402990e117586b491dc0973d398b
ashish9855 <a.maheshwari9855@gmail.com>
2018-03-28 14:05:52 +0200

    Create window view

---
+++ AKSwiftyLoader/AKSwiftyLoaderWindow.swift
@@ -0,0 +1,15 @@
+//
+//  AKSwiftyLoaderWindow.swift
+//  AKSwiftyLoader
+//
+//  Created by Ashish Maheshwari on 28/03/18.
+//  Copyright © 2018 Ashish Maheshwari. All rights reserved.
+//
+
+import UIKit
+
+class AKSwiftyLoaderWindow: UIView {
+
+    
+
+}
