b41a4752fac548881a3fc64b7e9e29215a0d00df
ashish9855 <a.maheshwari9855@gmail.com>
2018-03-28 15:06:00 +0200

    Add test class for loader window

---
+++ AKSwiftyLoaderTests/AKSwiftyLoaderWindow.swift
@@ -0,0 +1,24 @@
+//
+//  AKSwiftyLoaderWindow.swift
+//  AKSwiftyLoaderTests
+//
+//  Created by Ashish Maheshwari on 28/03/18.
+//  Copyright © 2018 Ashish Maheshwari. All rights reserved.
+//
+
+import XCTest
+
+class AKSwiftyLoaderWindow: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+}
