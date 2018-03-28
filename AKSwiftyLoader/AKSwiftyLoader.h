a58f59117f0bb6877e6cfde206402f3787581c27
Ashish <a.maheshwari9855@gmail.com>
2018-03-28 12:50:20 +0200

    Make project files

---
+++ AKSwiftyLoader/AKSwiftyLoader.h
@@ -0,0 +1,19 @@
+//
+//  AKSwiftyLoader.h
+//  AKSwiftyLoader
+//
+//  Created by Ashish Maheshwari on 28/03/18.
+//  Copyright © 2018 Ashish Maheshwari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AKSwiftyLoader.
+FOUNDATION_EXPORT double AKSwiftyLoaderVersionNumber;
+
+//! Project version string for AKSwiftyLoader.
+FOUNDATION_EXPORT const unsigned char AKSwiftyLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKSwiftyLoader/PublicHeader.h>
+
+
