88fe6f6122cc3fb709bc6bbf6416829a66cd0e2a
ashish9855 <a.maheshwari9855@gmail.com>
2018-03-28 15:04:39 +0200

    Initialize window view

--- AKSwiftyLoader/AKSwiftyLoaderWindow.swift
+++ AKSwiftyLoader/AKSwiftyLoaderWindow.swift
@@ -12,5 +12,28 @@ class AKSwiftyLoaderWindow: UIView {
 
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var loaderView: UIView!
-
+    
+    private override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    internal required init?(coder aDecoder: NSCoder) {
+        // For use in Interface Builder
+        super.init(coder: aDecoder)
+        setUpView()
+    }
+    
+    private func setUpView() {
+        
+        let contentView = getContentView()
+        addSubview(contentView)
+    }
+    
+    private func getContentView() -> UIView {
+        
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: AKSwiftyLoaderWindow.self), bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as! UIView
+    }
 }
