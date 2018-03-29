2801a14140ffd42b0aa0277352291cd0c6e86308
ashish9855 <a.maheshwari9855@gmail.com>
2018-03-29 14:41:27 +0200

    Create Loader Protocol

---
+++ AKSwiftyLoader/Protocols/LoaderProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  LoaderProtocol.swift
+//  AKSwiftyLoader
+//
+//  Created by Ashish Maheshwari on 29/03/18.
+//  Copyright © 2018 Ashish Maheshwari. All rights reserved.
+//
+
+import Foundation
