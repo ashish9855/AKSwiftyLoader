bd49029635b943d70e77ea942722e1e72911c7c4
ashish9855 <a.maheshwari9855@gmail.com>
2018-03-29 15:43:26 +0200

    Create AKSwiftyLoader class

---
+++ AKSwiftyLoader/AKSwiftyLoader.swift
@@ -0,0 +1,14 @@
+//
+//  AKSwiftyLoader.swift
+//  AKSwiftyLoader
+//
+//  Created by Ashish Maheshwari on 29/03/18.
+//  Copyright © 2018 Ashish Maheshwari. All rights reserved.
+//
+
+import Foundation
+
+class AKSwiftyLoader {
+    
+    
+}
